ed8bd92bbdb6a836cedfa9b9b491711083a6c7dd
Steve Winton <stevewinton@gmail.com>
2019-11-02 22:27:19 -0500

    Refresh all playlists

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import glob from 'glob';
+
 import getSpotifyClient from './spotify-client';
 import loadPlaylistSpec from './load-playlist-spec';
 
@@ -10,20 +12,25 @@ const refreshToken: string = process.env.SPOTIFY_REFRESH_TOKEN || '';
     // Get Spotify client
     const spotify = await getSpotifyClient(clientId, clientSecret, refreshToken);
 
-    // Load playlist spec
-    const playlist = loadPlaylistSpec('folk.yml');
+    glob('./playlists/*.yml', function(err, specs: string[]) {
+      // Iterate over all playlist specs
+      specs.forEach(async (spec) => {
+        // Load playlist spec
+        const playlist = loadPlaylistSpec(spec);
 
-    // Get recommendations
-    const { body: { tracks: recommendations } } = await spotify.getRecommendations(playlist.params);
+        // Get recommendations
+        const { body: { tracks: recommendations } } = await spotify.getRecommendations(playlist.params);
 
-    // Update playlist
-    const response = await spotify.replaceTracksInPlaylist(
-      playlist.id,
-      recommendations.map(
-        recommendation => recommendation.uri
-      )
-    );
-    console.log(response);
+        // Update playlist
+        const { body: response } = await spotify.replaceTracksInPlaylist(
+          playlist.id,
+          recommendations.map(
+            recommendation => recommendation.uri
+          )
+        );
+        console.log('snapshot_id: ', (response as {snapshot_id: string}).snapshot_id);
+      });
+    });
   } catch (e) {
     console.log(e);
     process.exit(1);
--- src/load-playlist-spec.ts
+++ src/load-playlist-spec.ts
@@ -2,10 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-const loadPlaylistSpec = function(filename: string) {
+const loadPlaylistSpec = function(path: string) {
   // Get document, or throw exception on error
   try {
-    return yaml.safeLoad(fs.readFileSync(path.join(__dirname, '..', 'playlists', filename), 'utf8'));
+    return yaml.safeLoad(fs.readFileSync(path, 'utf8'));
   } catch (e) {
     throw e;
   }
